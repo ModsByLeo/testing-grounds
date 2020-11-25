a738aa4a3d7317f8e81fe942999f2b717e01c9b8
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-11-25 12:06:29 +0200

    Initial commit

---
+++ src/main/java/adudecalledleo/testinggrounds/TestingGrounds.java
@@ -0,0 +1,10 @@
+package adudecalledleo.testinggrounds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestingGrounds implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/adudecalledleo/testinggrounds/client/TestingGroundsClient.java
@@ -0,0 +1,13 @@
+package adudecalledleo.testinggrounds.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TestingGroundsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

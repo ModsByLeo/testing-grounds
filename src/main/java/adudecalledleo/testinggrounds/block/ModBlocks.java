f01caad374dd503b0a2ed8bdbdb07d51d709cbe6
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-12-05 10:33:08 +0200

    let's try this again, shall we?

--- src/main/java/adudecalledleo/testinggrounds/block/ModBlocks.java
+++ src/main/java/adudecalledleo/testinggrounds/block/ModBlocks.java
@@ -1,22 +1,16 @@
 package adudecalledleo.testinggrounds.block;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-
-import static adudecalledleo.testinggrounds.TestingGrounds.id;
 
 public final class ModBlocks {
     private ModBlocks() { }
 
     public static void register() { /* clinit */ }
 
-    public static final MailboxBlock MAILBOX;
-
     static {
-        MAILBOX = register(id("mailbox"), new MailboxBlock(FabricBlockSettings.of(Material.WOOD)));
+
     }
 
     private static <T extends Block> T register(Identifier id, T block) {
--- src/main/java/adudecalledleo/testinggrounds/item/ModItems.java
+++ src/main/java/adudecalledleo/testinggrounds/item/ModItems.java
@@ -1,30 +1,19 @@
 package adudecalledleo.testinggrounds.item;
 
-import adudecalledleo.testinggrounds.block.ModBlocks;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-
-import static adudecalledleo.testinggrounds.TestingGrounds.id;
 
 public final class ModItems {
     private ModItems() { }
 
     public static void register() { /* clinit */ }
 
-    public static final BlockItem MAILBOX;
-
     static {
-        MAILBOX = register(id("mailbox"), new BlockItem(ModBlocks.MAILBOX, baseMCMailSettings()));
+
     }
 
     private static <T extends Item> T register(Identifier id, T item) {
         return Registry.register(Registry.ITEM, id, item);
-    }
-
-    private static FabricItemSettings baseMCMailSettings() {
-        return new FabricItemSettings().group(ModItemGroups.MCMAIL);
     }
 }

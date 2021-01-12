f5b061a230366814ceb634b6d94ade5903a805ea
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-01-12 21:32:58 +0200

    Testing Grounds: make Invincibility look a bit nicer

--- src/main/java/adudecalledleo/testinggrounds/effect/ModStatusEffects.java
+++ src/main/java/adudecalledleo/testinggrounds/effect/ModStatusEffects.java
@@ -8,7 +8,7 @@ import static adudecalledleo.testinggrounds.TestingGrounds.id;
 public final class ModStatusEffects {
     private ModStatusEffects() { }
 
-    public static final ModStatusEffect INVINCIBILITY = new ModStatusEffect(StatusEffectType.BENEFICIAL, 0x99453A);
+    public static final ModStatusEffect INVINCIBILITY = new ModStatusEffect(StatusEffectType.BENEFICIAL, 0x7073B4);
 
     public static void register() {
         Registry.register(Registry.STATUS_EFFECT, id("invincibility"), INVINCIBILITY);

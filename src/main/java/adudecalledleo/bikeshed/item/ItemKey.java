2c65960aac7dc4715154a7dd3a9d2e4a5439be6d
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-01-16 12:37:25 +0200

    Bikeshed: add some more methods to ItemKey

--- src/main/java/adudecalledleo/bikeshed/item/ItemKey.java
+++ src/main/java/adudecalledleo/bikeshed/item/ItemKey.java
@@ -2,7 +2,9 @@ package adudecalledleo.bikeshed.item;
 
 import adudecalledleo.bikeshed.tag.ImmutableCompoundTag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -10,12 +12,22 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Objects;
 
 public final class ItemKey {
+    public static final @NotNull ItemKey EMPTY = of(Items.AIR);
+
     public static @NotNull ItemKey of(@NotNull Item item, @Nullable CompoundTag tag) {
         return new ItemKey(item, tag == null ? null : ImmutableCompoundTag.copyOf(tag));
     }
 
     public static @NotNull ItemKey of(@NotNull Item item) {
         return new ItemKey(item, null);
+    }
+
+    public static @NotNull ItemKey of(@NotNull ItemConvertible itemConvertible, @Nullable CompoundTag tag) {
+        return of(itemConvertible.asItem(), tag);
+    }
+
+    public static @NotNull ItemKey of(@NotNull ItemConvertible itemConvertible) {
+        return of(itemConvertible.asItem());
     }
 
     private final @NotNull Item item;
@@ -30,18 +42,41 @@ public final class ItemKey {
         return item;
     }
 
+    public boolean hasTag() {
+        return tag != null;
+    }
+
     public @Nullable ImmutableCompoundTag getTag() {
         return tag;
     }
 
+    public @Nullable CompoundTag copyTag() {
+        if (tag == null)
+            return null;
+        return tag.copy();
+    }
+
+    public boolean isEmpty() {
+        return this == EMPTY || item == Items.AIR;
+    }
+
     public boolean matches(@NotNull ItemStack stack) {
+        if (stack.isEmpty())
+            return isEmpty();
         if (item != stack.getItem())
             return false;
-        if (tag == null && !stack.hasTag())
-            return true;
-        if (tag == null && stack.hasTag() || tag != null && !stack.hasTag())
-            return false;
-        return tag != null && tag.equals(stack.getTag());
+        if (stack.hasTag())
+            return tag != null && tag.equals(stack.getTag());
+        else
+            return tag == null;
+    }
+
+    public @NotNull ItemStack toStack(int count) {
+        if (isEmpty())
+            return ItemStack.EMPTY;
+        ItemStack stack = new ItemStack(item, count);
+        stack.setTag(copyTag());
+        return stack;
     }
 
     @Override

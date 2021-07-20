7d84b3ca548e205b9ca344b244584ed726d6192c
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-20 23:32:39 +0300

    Remove Entity Events since that's a SpeedbridgeMC project now

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Welcome to the most cursed GitHub repository ever. _Please locate the nearest ex
 ### What's in here?
 * **Craftdown** - Markdown AST parser with Text renderer (and the other way around), `adudecalledleo.craftdown`
 * **Server-Siding** - server-side mod util library, `adudecalledleo.serversiding`
-* **Entity Events** - Using extremely cursed techniques to give you ticks and damage, `adudecalledleo.entityevents`
 * **Testing Grounds** - general testing stuff, `adudecalledleo.testinggrounds`
 
 ### Can I use it?
--- src/main/java/adudecalledleo/testinggrounds/TestingGrounds.java
+++ src/main/java/adudecalledleo/testinggrounds/TestingGrounds.java
@@ -1,14 +1,10 @@
 package adudecalledleo.testinggrounds;
 
-import adudecalledleo.entityevents.api.EntityDamageEvents;
-import adudecalledleo.entityevents.api.EntityTickEvents;
 import adudecalledleo.testinggrounds.block.ModBlocks;
 import adudecalledleo.testinggrounds.block.entity.ModBlockEntities;
 import adudecalledleo.testinggrounds.command.ModCommands;
-import adudecalledleo.testinggrounds.effect.ModStatusEffects;
 import adudecalledleo.testinggrounds.item.ModItemGroups;
 import adudecalledleo.testinggrounds.item.ModItems;
-import adudecalledleo.testinggrounds.potion.ModPotions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.entity.EntityType;
@@ -30,44 +26,11 @@ public final class TestingGrounds implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Initializing");
 
-        ModStatusEffects.register();
-        ModPotions.register();
         ModBlocks.register();
         ModBlockEntities.register();
         ModItemGroups.register();
         ModItems.register();
         ModCommands.register();
-
-        EntityDamageEvents.of(EntityType.CREEPER).registerAfter((target, source, amount) -> {
-            // if damage won't kill creeper, ignite it
-            if (amount < target.getHealth())
-                target.ignite();
-        });
-        EntityDamageEvents.living().registerBefore((target, source, amount) -> {
-            // if entity has Invincibility effect, cancel damage event
-            if (target.hasStatusEffect(ModStatusEffects.INVINCIBILITY))
-                return TriState.TRUE;
-            return TriState.DEFAULT;
-        });
-        EntityDamageEvents.ofClass(TameableEntity.class).registerBefore((target, source, amount) -> {
-            // don't let owners damage their own pets
-            if (target.getOwnerUuid() != null && source.getAttacker() != null) {
-                if (target.getOwnerUuid().equals(source.getAttacker().getUuid()))
-                    return TriState.TRUE;
-            }
-            return TriState.DEFAULT;
-        }).registerCancelled((target, source, amount) -> {
-            if (target.getOwnerUuid() != null && source.getAttacker() instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) source.getAttacker();
-                if (target.getOwnerUuid().equals(player.getUuid()))
-                    player.sendMessage(new LiteralText("Prevented own pet damage!"), true);
-            }
-        });
-        EntityTickEvents.inTag(EntityTypeTags.ARROWS).registerAfter(entity -> {
-            // make arrows a bit floatier
-            if (!entity.isOnGround())
-                entity.addVelocity(0, 0.025, 0);
-        });
     }
 
     public static Identifier id(String path) {
